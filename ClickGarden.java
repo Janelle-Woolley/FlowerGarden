ecb233a05c74f5409229fa7b23f71d2d6d2d23d1
Janelle-Woolley <103225532+Janelle-Woolley@users.noreply.github.com>
2023-03-14 09:27:11 +1300

    creates an array to holds flowers

---
+++ ClickGarden.java
@@ -0,0 +1,41 @@
+import ecs100.*;
+import java.awt.Color;
+/**
+ * Create a garden with each flower in an array
+ * Can click on flowers to make them grow
+ *
+ * @author Janelle Woolley
+ * @version 1.0 14/03/2023
+ */
+public class ClickGarden
+{
+    // class constants
+    private static final int STARTX = 75;
+    private static final int YPOS = 100;
+    private static final int MAXFLOWERS = 5;
+    
+    // make an array of flowers
+    private Flower[] flowerBed = new Flower[MAXFLOWERS]; //intialise array
+    
+    /**
+     * Constructor for objects of class ClickGarden
+     */
+    public ClickGarden()
+    {
+        // initialise instance variables
+        UI.initialise();
+        UI.addButton("Quit", UI::quit);
+        
+        //set up the flowers
+        for (int i = 0; i < MAXFLOWERS; i++) {
+            // set every flower to a random colour
+            Color col = new Color((float)Math.random(), (float)Math.random(), (float)Math.random());
+            flowerBed[i] = new Flower(STARTX*(i+1), YPOS, 10, 50, col); // create the flower objs in the array
+        }
+        
+        // draw each flower
+        for (Flower flower : flowerBed) {
+            flower.draw();
+        }
+    }
+}
